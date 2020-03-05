b3edb4113bb7a5bc35c0d45c929fff2cae40fb83
Albert Kozak <albert.kozak@live.com>
2020-03-04 19:11:02 -0800

    Implemented API functionality for text view

--- app/src/main/java/com/albertkozak/nooz/MainActivity.kt
+++ app/src/main/java/com/albertkozak/nooz/MainActivity.kt
@@ -2,9 +2,14 @@ package com.albertkozak.nooz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.albertkozak.nooz.network.Article
 import com.albertkozak.nooz.network.ArticleResponse
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
@@ -14,6 +19,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // We will use a coroutine to help us simplify the asynchronous code
+        getPopularArticles()
     }
 
     // Moshi to help parse our JSON response into kotlin class objects
@@ -37,5 +45,16 @@ class MainActivity : AppCompatActivity() {
         @GET("mostpopular/v2/viewed/1.json")
         suspend fun getPopularArticlesAsync(@Query("api-key") apiKey: String): ArticleResponse
     }
+
+    // This is our coroutine (by adding GlobalScope launch Main)
+    private fun getPopularArticles() = GlobalScope.launch(Dispatchers.Main){
+        // This implements the interface asynchronously
+        // without blocking the main thread and potentially freezing our app
+        val firstResult: Article = api.getPopularArticlesAsync(BuildConfig.API_KEY).results.first()
+
+        // Upon completion of the HTTP request we update the layout element
+        introText.text = firstResult.title
+    }
+
 
 }
