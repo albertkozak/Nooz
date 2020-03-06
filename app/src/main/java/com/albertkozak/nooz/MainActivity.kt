a4e8c4dc337ce3cbaf9da1f6d3ca0f8343160e06
Albert Kozak <albert.kozak@live.com>
2020-03-05 18:31:20 -0800

    Added recyclerview, article images, swipe feature, and webview

--- app/src/main/java/com/albertkozak/nooz/MainActivity.kt
+++ app/src/main/java/com/albertkozak/nooz/MainActivity.kt
@@ -1,38 +1,70 @@
 package com.albertkozak.nooz
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.albertkozak.nooz.network.Article
-import com.albertkozak.nooz.network.ArticleResponse
-import com.albertkozak.nooz.network.NewsAPI
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
-import retrofit2.http.GET
-import retrofit2.http.Query
+import kotlinx.android.synthetic.main.item_article.view.*
+import org.koin.android.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
-    private val api = NewsAPI()
-    private var articles: List<Article> = listOf()
+
+    private val articlesViewModel: ArticlesViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // We will use a coroutine to help us simplify the asynchronous code
-        loadPopularArticles()
-    }
 
-    private fun loadPopularArticles() = GlobalScope.launch(Dispatchers.Main) {
-        val popularArticles = api.getPopularArticles()
-        if(popularArticles != null) {
-            articles = popularArticles
-            introText.text = articles.first().title
+        article_list.layoutManager = GridLayoutManager(this, 2)
+
+        articlesViewModel.loadPopularArticles()
+
+        articlesViewModel.articles.observe(this, Observer {articles ->
+            articles ?: return@Observer
+            article_list.adapter = ArticleAdapter(articles, this, articleSelected = {
+                val intent = ArticleActivity.newIntent(it, this)
+                startActivity(intent)
+            })
+        })
+
+        swipe_refresh_articles_layout.setOnRefreshListener {
+            articlesViewModel.loadPopularArticles()
+            swipe_refresh_articles_layout.isRefreshing = false
         }
     }
 }
+
+private class ArticleAdapter(private val articles: List<Article>, val context: Context, val articleSelected: (Article) -> Unit): RecyclerView.Adapter<ArticleViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleViewHolder {
+        return ArticleViewHolder(LayoutInflater.from(context).inflate(R.layout.item_article, parent, false))
+    }
+
+    override fun getItemCount(): Int {
+        return articles.count()
+    }
+
+    override fun onBindViewHolder(holder: ArticleViewHolder, position: Int) {
+        val article = articles[position]
+
+        holder.itemView.setOnClickListener {
+            articleSelected(article)
+        }
+
+        holder.itemView.article_title.text = article.title
+        article.media.firstOrNull()?.mediaMetadata?.firstOrNull()?.url.let {
+            Glide.with(context).load(it).into(holder.itemView.article_image)
+        }
+    }
+}
+
+private class ArticleViewHolder(view: View): RecyclerView.ViewHolder(view)
